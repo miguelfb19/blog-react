b6d69bfe6aea4a07ff0bda0774a5f8133c0922b5
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-03-06 11:16:05 -0500

    pruebas

--- src/services/get-images-from-s3.js
+++ src/services/get-images-from-s3.js
@@ -2,6 +2,7 @@ import Global from "../components/Global";
 import axios from "axios";
 
 const url = Global.url;
+
 export const getImagesFromDS3 = async (articleImg) => {
   const imgUrl = await axios.get(url + "get-image/" + articleImg);
   return imgUrl.data;
